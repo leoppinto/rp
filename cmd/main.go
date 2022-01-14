3d616ca32729742f7561811aaf168eab23a498d5
leonardo.pinto <leonardo.pinto@picpay.com>
2022-01-14 16:02:14 -0300

    refactory: Organização das pastas

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "rp"
+import (
+	"rp/server"
+)
 
 func main() {
-	rp.StartServer()
+	server.StartServer()
 }
--- server/start.go
+++ server/start.go
@@ -1,4 +1,4 @@
-package rp
+package server
 
 import (
 	"google.golang.org/grpc"
@@ -13,22 +13,17 @@ var ProductsList = model.NewProducts()
 var JobConfigurationService = model.NewJobResouceConfigurations()
 
 func StartServer() {
-
 	lis, err := net.Listen("tcp", "192.168.0.15:8300")
 	if err != nil {
 		log.Print("[ERROR] Grpc Server not connected")
 	}
 	grpcServer := grpc.NewServer()
-
 	productService := service.NewProductGrpcServer(ProductsList)
 	pb.RegisterProductServiceServer(grpcServer, productService)
-
 	jobConfigurationService := service.NewJobResourceConfigurationGrpcServer(JobConfigurationService)
 	pb.RegisterJobResourceConfigurationServiceServer(grpcServer, jobConfigurationService)
-
 	log.Print("[INFO] Server Gprc Connected")
 	log.Print("[INFO] Job Configuration Service Started")
 	log.Print("[INFO] Product Service Started")
-
 	grpcServer.Serve(lis)
 }
